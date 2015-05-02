b98b1afdae60fb84d069ea6162cc1cb027699b1f
uazim <uzma_aazim@yahoo.com>
2015-05-02 14:45:04 -0500

    copying the blogpart1

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM openshift/ruby-20-centos
+RUN gem install sinatra
+ADD app.rb /tmp/app.rb
+EXPOSE 8080
+CMD ruby /tmp/app.rb
---
+++ app.rb
@@ -0,0 +1,12 @@
+require 'sinatra'
+
+set :bind, '0.0.0.0'
+set :port,8080
+get '/' do
+  "Modified hello world!\n"+
+# ENV values are generated during template processing
+# and then passed to the container when openshift launches it.
+  "User is #{ENV['ADMIN_USERNAME']}\n"+
+  "Password is #{ENV['ADMIN_PASSWORD']}\n"+
+  "DB password is #{ENV['DB_PASSWORD']}\n"
+end
